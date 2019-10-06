9f107c42d1e449de7bb2bdcd7a09e509c0c9a1e3
Hugh O'Reilly <horeilly1101@gmail.com>
2019-10-06 13:09:14 -0500

    reorganized the game

--- countdown/countdown_game.py
+++ countdown/countdown_game.py
@@ -20,19 +20,34 @@ class CountdownGame:
         print(RULES)
         self.play_round(0)
 
+    def end(self):
+        print(END_GAME)
+
     def play_round(self, round_num):
+        # initialize the game state for the round
         cards = self._deck.draw_six_cards()
         goal_expression, goal = self._compute_goal()
 
+        # get the user's answer
         response = input(START_ROUND(round_num, cards, goal))
         expression = self._parser.parse(response)
 
+        # check validity
         if not self._check_validity(expression, cards):
             print(INVALID_INPUT)
             return
 
+        # figure out whether or not the answer was correct
         result = expression.evaluate()
-        result_message = f"""
-        You submitted {expression} = {result}.
-        Our solution was {goal_expression} = {result}.
-        """
+        if result == goal:
+            print(CORRECT_ANSWER(expression, result))
+        else:
+            print(INCORRECT_ANSWER(expression, result, goal_expression, goal))
+
+        # stop playing after 5 rounds, or when the user wants to quit
+        if round_num < 4:
+            keep_playing = input(CONTINUE_PLAYING)
+            if keep_playing.strip() == "y":
+                self.play_round(round_num + 1)
+
+        self.end()
