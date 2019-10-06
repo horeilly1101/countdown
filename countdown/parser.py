b51b62db0369408e1d47be8ba90d3b5a9589a07c
Hugh O'Reilly <horeilly1101@gmail.com>
2019-10-06 00:38:33 -0500

    added some more unit tests

--- countdown/parser.py
+++ countdown/parser.py
@@ -1,6 +1,6 @@
 
 from lark import Lark, Transformer
-from countdown.expression import Add, Multiply, Subtract, Divide, Number
+from countdown.expression import Add, Multiply, Subtract, Divide, Number,  Expression
 
 
 # --------------------
@@ -41,7 +41,7 @@ class _TreeToExpression(Transformer):
     def expr(self, values):
         """
         Convert the expr's from the grammar definition into Adds and
-        Multiplies.
+        Subtracts.
 
         :param values: supplied by parser
         :return: Expression
@@ -57,6 +57,13 @@ class _TreeToExpression(Transformer):
         return num
 
     def term(self, values):
+        """
+        Convert the term's from the grammar definition into Multiplies and
+        Divides.
+
+        :param values: supplied by parser
+        :return: Expression
+        """
         if len(values) == 3:
             factor1, op, factor2 = values
             if op == "*":
@@ -68,6 +75,12 @@ class _TreeToExpression(Transformer):
         return num
 
     def base(self, values):
+        """
+        Convert the bases's from the grammar definition into Numbers.
+
+        :param values: supplied by parser
+        :return: Expression
+        """
         if len(values) == 3:
             lparen, expr, rparen = values
             return expr
@@ -77,10 +90,20 @@ class _TreeToExpression(Transformer):
 
 
 class Parser:
+    """
+    Class that allows us to parse strings into Expression objects.
+    This functions as a wrapper around the messy Lark code.
+    """
     def __init__(self):
         self._parser = _expression_parser
         self._transformer = _TreeToExpression()
 
-    def parse(self, expression_str):
+    def parse(self, expression_str) -> Expression:
+        """
+        Parse the input string into an Expression.
+
+        :param expression_str: input string
+        :return: a valid Expression
+        """
         tree = self._parser.parse(expression_str)
         return self._transformer.transform(tree)
--- tests.py
+++ tests.py
@@ -15,17 +15,32 @@ class TestParser(unittest.TestCase):
         self.assertEqual(26, result2.evaluate())
 
     def test_subtract(self):
-        pass
+        result1 = self.parser.parse("9-5")
+        self.assertEqual(4, result1.evaluate())
+
+        result2 = self.parser.parse("67-9")
+        self.assertEqual(58, result2.evaluate())
 
     def test_multiply(self):
-        pass
+        result1 = self.parser.parse("5*5*5")
+        self.assertEqual(125, result1.evaluate())
+
+        result2 = self.parser.parse("5*14")
+        self.assertEqual(70, result2.evaluate())
 
     def test_divide(self):
-        pass
+        result1 = self.parser.parse("5/5")
+        self.assertEqual(1, result1.evaluate())
+
+        result2 = self.parser.parse("14/7")
+        self.assertEqual(2, result2.evaluate())
 
     def test_parentheses(self):
         result1 = self.parser.parse("((5+5)+(5+7))")
         self.assertEqual(22, result1.evaluate())
+
+        result2 = self.parser.parse("(5*7)+9")
+        self.assertEqual(44, result2.evaluate())
 
     def test_order_of_operations(self):
         pass
